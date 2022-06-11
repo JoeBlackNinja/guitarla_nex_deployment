33f87df1c708bc89cc6898bf1b80e0e2131d6af8
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-06-11 16:54:59 -0500

    PUERTO nuevo

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', env.HOST),
-  port: env.int('PORT', env.PORT),
+  port: env.int('PORT', 5003),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', env.ADMIN_JWT_SECRET),
